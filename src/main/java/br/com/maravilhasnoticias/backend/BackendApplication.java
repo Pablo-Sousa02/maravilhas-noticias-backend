6598161223f3b18da686249bd0235deea854a09c
Pablo-Sousa02 <pablooficial22@hotmail.com>
2026-09-25 15:44:51 -0300

    fix: stabilize paginated API responses

--- src/main/java/br/com/maravilhasnoticias/backend/BackendApplication.java
+++ src/main/java/br/com/maravilhasnoticias/backend/BackendApplication.java
@@ -3,9 +3,12 @@ package br.com.maravilhasnoticias.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class BackendApplication {
 
 	public static void main(String[] args) {
